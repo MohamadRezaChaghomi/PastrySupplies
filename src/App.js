05fc31d1fb16b08a72c359367254969870dda7ee
mahdiamiri1208 <mahdiamiri1208@gmail.com>
2025-08-01 02:17:12 +0330

    update social icons

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ export default function App() {
 
   return (
     <>
-      <InfoNotice />
+      <Footer />
     </>
   );
 }
--- src/components/Footer/social-email/SocialAndEmail.jsx
+++ src/components/Footer/social-email/SocialAndEmail.jsx
@@ -1,17 +1,20 @@
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-import TwitterIcon from "@mui/icons-material/Twitter";
+import XIcon from "../../../assets/images/icons8-x-logo-32.png";
 import "./SocialAndEmail.css";
 
 const SocialAndEmail = () => (
   <div className="social-email">
     <h5 className="text-social-email">همراه ما باشید!</h5>
     <div className="social-icons">
-      <TwitterIcon sx={{ fontSize: { xs: 32, lg: 38 } }} />
-      <WhatsAppIcon sx={{ fontSize: { xs: 32, lg: 38 } }} />
-      <TelegramIcon sx={{ fontSize: { xs: 32, lg: 38 } }} />
-      <InstagramIcon sx={{ fontSize: { xs: 32, lg: 38 } }} />
+      <img
+        src= {XIcon}
+        alt="X Icon"
+      ></img>
+      <WhatsAppIcon sx={{ fontSize: { xs: 32, lg: 38 }, color: "#075E54" }} />
+      <TelegramIcon sx={{ fontSize: { xs: 32, lg: 38 }, color: "#0088cc" }} />
+      <InstagramIcon sx={{ fontSize: { xs: 32, lg: 38 }, color: "#E1306C" }} />
     </div>
     <h5 className="text-social-email">
       با ثبت ایمیل، از جدید ترین تخفیف ها باخبر شوید
